70e1acd3d56ebe1186da17ba8ee1678a5bb9a68e
carmina000 <carminayanez7@gmail.com>
2025-12-07 01:25:29 +0800

    Update all project files

--- poverty_prediction/poverty-backend/app.py
+++ poverty_prediction/poverty-backend/app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from fastapi import FastAPI
 import pickle
 import numpy as np
 import os
 
-app = Flask(__name__)
-CORS(app)
+app = FastAPI()
 
 # Load the model
 try:
